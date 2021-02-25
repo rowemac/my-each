246e743db7c9f1b89e1737cbd5c33bd88667975e
Mac Rowe <m.rowe42@gmail.com>
2021-02-25 14:15:01 -0500

    commit tree 13049181e84f970a0e4783b38d72707b8e1c312c

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+
+def my_each(words)
+
+  i = 0
+  collect = []
+  
+  while i < words.length
+      collect << yield(words[i])
+      i = i + 1
+    end
+  return words
+
 end
